6c0f6cab26b960477c66d92fa885bec34dfee0a8
ajfriesen <andre.friesen@gmail.com>
2018-06-10 17:27:57 +0200

    Stay with gorilla mux router

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
@@ -17,9 +16,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact</h1>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>FAQ</h1>"+"<p>These are the frequently asked questions:")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
