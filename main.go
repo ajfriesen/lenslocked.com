d2169c93195d8941c7b0c834824f2a53e088bf1a
Andrej Friesen <andre.friesen@gmail.com>
2018-06-18 22:25:56 +0200

    Wip

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"lenslocked.com/views"
@@ -28,29 +27,12 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>FAQ</h1>"+"<p>These are the frequently asked questions:")
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>FAQ</h1>"+"<p>These are the frequently asked questions:")
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>404</h1>"+"<p>Not found")
-}
-
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 
 	r := mux.NewRouter()
-	r.NotFoundHandler = nf
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
