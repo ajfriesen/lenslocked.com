207bc8d65f7a09da2d42f2e10337d40d9769e1bd
ajfriesen <andre.friesen@gmail.com>
2018-06-20 23:39:41 +0200

    Build controller for pasing form data

--- controllers/users.go
+++ controllers/users.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/gorilla/schema"
 
 	"lenslocked.com/views"
 )
@@ -15,6 +18,26 @@ func NewUsers() *Users {
 
 type Users struct {
 	NewView *views.View
+}
+
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
+// Create will process the signup form when a user submits the form.
+// This is used to create a new user account.
+// POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+	dec := schema.NewDecoder()
+	form := SignupForm{}
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 }
 
 //New is used to render the form where a user can create a new user account.
