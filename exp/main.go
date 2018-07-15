a7d74060af91f068bdf11e4fac184a8e5146a5bd
ajfriesen <andre.friesen@gmail.com>
2018-07-15 23:19:02 +0200

    WIP

--- exp/main.go
+++ exp/main.go
@@ -8,25 +8,37 @@ import (
 )
 
 const (
-	host = "localhost"
-	port = 5432
-	user = "postgres"
-	// password = ""
-	dbname = "lenslocked_dev"
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "lenslocked_dev"
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
-		host, port, user, dbname)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
+	defer db.Close()
 
-	fmt.Println("Successfully connected!")
-	db.Close()
+	for i := 1; i <= 6; i++ {
+		userID := 1
+		if i > 3 {
+			userID = 2
+		}
+		amount := i * 100
+		description := fmt.Sprintf("USB-C Adapter x%d", i)
+
+		rows, err := db.Query(`
+			SELECT * FROM users
+			INNER JOIN orders ON users.id=orders.user_id
+			INSERT INTO orders(user_id, amount, description)
+			VALUES($1,$2,$3)`, userID, amount, description)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
